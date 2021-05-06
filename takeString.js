486b3a054c517fc85502e13d6fb676d2e2be2ff3
alienao <armin.orujyan@gmail.com>
2021-05-06 21:37:14 +0400

    build: takeString.js

---
+++ takeString.js
@@ -0,0 +1,15 @@
+//2. Write a function redundant that takes in a string str and returns a function thatreturns str.
+// const f1 = redundant("apple");
+// //f1() ➞ "apple"
+// const f2 = redundant("pear");
+// //f2() ➞ "pear"
+// const f3 = redundant("");
+// //f3() ➞ ""
+
+function redundant(str){
+    return function(){
+        return str;
+    }
+}
+const f1 = redundant("apple");
+console.log(f1());
